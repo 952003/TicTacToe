de08f51aacea1cbd7cf26b81aae2a618b2911f97
952003 <51296871+952003@users.noreply.github.com>
2022-02-09 21:41:24 +0300

    Add project files.

---
+++ TicTacToe/Hubs/MainHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Hubs
+{
+    public class MainHub : Hub
+    {
+        public static Dictionary<string, (int, int)> userMove = new Dictionary<string, (int, int)>();
+
+        public async Task MakeMove(int x, int y)
+        {
+            userMove.Add(Context.ConnectionId, (x, y));
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
